717e11c4960a5c4f966e031aff1c5f2bda8f9b86
Manoj Solanki <ac5882@coventry.ac.uk>
2017-11-15 11:09:20 +0000

    Updated version.

--- version.php
+++ version.php
@@ -30,12 +30,12 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_news_slider';
 
 // YYYYMMDDHH (year, month, day, 24-hr time).
-$plugin->version = 2017111400;
+$plugin->version = 2017111500;
 
 // YYYYMMDDHH (This is the release version for Moodle 3.2).
 $plugin->requires = 2016120500;
 
 $plugin->dependencies = array('theme_adaptable' => 2017053100);
 
-$plugin->maturity = MATURITY_BETA;
-$plugin->release = "1.1";
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = "1.2";
